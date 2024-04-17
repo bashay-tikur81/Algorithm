6d2ee6cf7c961f095e552ec91cb414516c5f179c
Ertale81 <tikurbashay81@gmail.com>
2024-04-17 23:01:56 +0300

    Binary Search Tree

---
+++ Day06/binary_search_tree.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define true 1
+#define false 0
+
+typedef struct bst_node {
+    int data;
+    struct bst_node *left;
+    struct bst_node *right;
+} bst_node;
+
+
+bst_node* give_me_node(int);
+bst_node* insert(bst_node*, int);
+int search(bst_node *, int);
+
+bst_node *root;
+
+int main(){
+    int num, i = 1, counter;
+    root = NULL;
+    printf("How many data to enter: ");
+    scanf("%d", &counter);
+    while (i <= counter){
+	printf("Number %d: ", i);
+	scanf("%d", &num);
+	root = insert(root, num);
+	++i;
+	
+    }
+    printf("Now time for searching...\n");
+    printf("A number to be  searched: ");
+    scanf("%d", &num);
+    printf("%d Does %s exist in the tree.\n", num, (search(root, num))? "":"not");
+}
+
+bst_node* insert(bst_node *root, int data){
+    if (root == NULL){
+	root = give_me_node(data);
+    }
+    else if (data <= root->data){
+	root->left = insert(root->left, data);
+    }
+    else {
+	root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+int search(bst_node* root, int data){
+    if (root == NULL){
+	return false;
+    }
+    if (root->data == data){
+	return true;
+    }
+    else if (data <= root->data){
+	return search(root->left, data);
+    }
+    else {
+	return search(root->right, data);
+    }
+}
+bst_node* give_me_node(int value){
+    bst_node* new_node = (bst_node*) malloc(sizeof(bst_node));
+    if (new_node == NULL){
+	printf("Can't allocate memory.\n");
+	exit(1);
+    }
+    new_node->data = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
